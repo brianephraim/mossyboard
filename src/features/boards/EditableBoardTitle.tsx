bd039c0e6ecf01d890028be7b1c29babf78a5ddf
Brian Ephraim <brianephraim@gmail.com>
2026-04-26 11:06:10 -0400

    cleanup

--- src/features/boards/EditableBoardTitle.tsx
+++ src/features/boards/EditableBoardTitle.tsx
@@ -63,20 +63,15 @@ export function EditableBoardTitle({
           color="$boardHeading"
           fontSize="$10"
           fontWeight="800"
-          // lineHeight={52}
           height={72}
-          // minHeight={72}
           borderWidth={1}
           borderRadius="$4"
           borderColor="transparent"
           backgroundColor="transparent"
           boxShadow="transparent 0px 0px 0px 0px"
           paddingHorizontal={10}
-          // paddingTop={14}
-          // paddingBottom={14}
           marginHorizontal={-10}
           marginTop={-4}
-          // marginBottom={8}
           focusStyle={{ outlineWidth: 0 }}
           focusVisibleStyle={{
             outlineWidth: 0,
